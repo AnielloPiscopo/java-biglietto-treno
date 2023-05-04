13d2e274f30b03506d5abf57a44b4bbfa7b8bf99
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-04 15:29:43 +0200

    Finished the main trace of the second exercise

---
+++ src/org/lessons/java/CibiPreferiti.java
@@ -0,0 +1,11 @@
+package org.lessons.java;
+
+public class CibiPreferiti {
+public static void main(String[] args) {
+	String[] favouriteFoods = {"pizza" , "pasta" , "lasagne" , "patatine" , "nutella"};
+	int favoutiteFoodsListLength = favouriteFoods.length;
+	System.out.println(favoutiteFoodsListLength);
+	System.out.println(favouriteFoods[0]);
+	System.out.println(favouriteFoods[favoutiteFoodsListLength - 1]);
+}
+}
