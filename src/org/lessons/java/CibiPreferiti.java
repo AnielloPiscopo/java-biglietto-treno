a015d38a5baa7b25273e5d77407f0ee5c2d33646
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-04 15:37:18 +0200

    Finished the bonus trace of the second exercise

--- src/org/lessons/java/CibiPreferiti.java
+++ src/org/lessons/java/CibiPreferiti.java
@@ -4,8 +4,14 @@ public class CibiPreferiti {
 public static void main(String[] args) {
 	String[] favouriteFoods = {"pizza" , "pasta" , "lasagne" , "patatine" , "nutella"};
 	int favoutiteFoodsListLength = favouriteFoods.length;
-	System.out.println(favoutiteFoodsListLength);
-	System.out.println(favouriteFoods[0]);
-	System.out.println(favouriteFoods[favoutiteFoodsListLength - 1]);
+	System.out.println("Nella mia lista ho " + favoutiteFoodsListLength + "cibi preferiti");
+	System.out.println("Il mio cibo prefetito è: " + favouriteFoods[0]);
+	System.out.println("Il cibo che mi piace di meno è: " + favouriteFoods[favoutiteFoodsListLength - 1]);
+	
+	if(favoutiteFoodsListLength%2 != 0) {
+		System.out.println("Il cibo che sta nel mezzo è: " + favouriteFoods[favoutiteFoodsListLength/2]);
+	}else {
+		System.out.println("I cibi che stanno nel mezzo sono " + favouriteFoods[favoutiteFoodsListLength/2 - 1] + " e " + favouriteFoods[favoutiteFoodsListLength/2]);
+	}
 }
 }
