8e8d33ccbce6de0bda8b2efaa9126d0a49a7fb08
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-04 15:22:31 +0200

    Finished the main trace

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,7 +1,28 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 public static void main(String[] args) {
+	String kmToTravel;
+	String userAge;
 	
+	float ticketPriceMoltiplicatorUnit = 0.21f;
+	
+	Scanner in = new Scanner(System.in);
+	System.out.print("Quanti km dovrai percorrere? ");
+	kmToTravel = in.nextLine();
+	int km = Integer.parseInt(kmToTravel);			
+	float ticketPriceWithoutDiscount = km*ticketPriceMoltiplicatorUnit;
+	
+	System.out.print("Quanti anni hai ? ");
+	userAge = in.nextLine();
+	int userAgeNum =Integer.parseInt(userAge);
+	
+	float discount = (userAgeNum < 18) ? ticketPriceWithoutDiscount/100 * 20 : (userAgeNum > 65) ? ticketPriceWithoutDiscount/100 * 40 : 0;
+	float ticketPrice = ticketPriceWithoutDiscount - discount;
+	
+	System.out.println(ticketPrice);
+	in.close();
 }
 }
