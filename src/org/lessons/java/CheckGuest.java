d123c0636edaee81c9dd68dee78496934c6e87b6
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-04 18:00:21 +0200

    Correcetd the fizzbuzz and completed the last exercise with the bonus

--- src/org/lessons/java/CheckGuest.java
+++ src/org/lessons/java/CheckGuest.java
@@ -1,5 +1,36 @@
 package org.lessons.java;
 
-public class CheckGuest {
+import java.util.Scanner;
 
+public class CheckGuest {
+	public static void main(String[] args) {
+		// Data
+		String[] guestsList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		
+		// Receiving data in input
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserisci il tuo nome: ");
+		String userName = in.nextLine();
+		in.close();
+		
+		//First method: for
+		for(int i=0 ; i<guestsList.length; i++) {
+			if(userName.equals(guestsList[i])) {
+				System.out.println("Ok puoi entrare");
+				return;
+			}
+		}
+		
+		System.out.println("Non puoi entrare");
+		
+		//Second method: while
+		int i = 0;
+		while(i<guestsList.length) {
+			if(userName.equals(guestsList[i])) {
+				System.out.println("Ok puoi entrare");
+				return;
+			}
+			i++;
+		}
+	}
 }
--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,27 +1,28 @@
 package org.lessons.java;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class FizzBuzz {
 	public static void main(String[] args) {
+		// Main Trace
 		int minValue = 1;
 		int maxValue = 100;
-		Random rnd = new Random();
-		int randomNumber = rnd.nextInt(minValue , maxValue);
-		System.out.println("Il numero generato casualmente che va da 1 a 100 è: " + randomNumber);
-		String response = (randomNumber%3 == 0 && randomNumber%5 == 0) ? "FizzBuzz" : (randomNumber%3 == 0) ? "Fizz" : (randomNumber%5 == 0) ? "Buzz" : "Niente FizzBuzz";
-		System.out.println(response);
+		for(int i=minValue ; i<maxValue ; i++) {
+			String response = (i%3 == 0 && i%5 == 0) ? "FizzBuzz" : (i%3 == 0) ? "Fizz" : (i%5 == 0) ? "Buzz" : ""+i;
+			System.out.println(response);
+		}
 		
+		
+		// Bonus
 		Scanner in = new Scanner(System.in);
 		do {
 			System.out.print("Digita il nuovo valore massimo: ");
 			maxValue = in.nextInt();
-			randomNumber = rnd.nextInt(minValue , maxValue);
-			System.out.println("Il numero generato stando al limite dato da te è: " + randomNumber);
-			response = (randomNumber%3 == 0 && randomNumber%5 == 0) ? "FizzBuzz" : (randomNumber%3 == 0) ? "Fizz" : (randomNumber%5 == 0) ? "Buzz" : "Niente FizzBuzz";
-			System.out.println(response);
-		}while(randomNumber <= 1);
+			for(int i=minValue ; i<=maxValue ; i++) {
+				String response = (i%3 == 0 && i%5 == 0) ? "FizzBuzz" : (i%3 == 0) ? "Fizz" : (i%5 == 0) ? "Buzz" : ""+i;
+				System.out.println(response);
+			}
+		}while(maxValue <= 1);
 		in.close();
 	}
 }
