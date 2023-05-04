0e25c7e86e3dbd364ad0c50b0bea680642272cc8
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-04 16:57:41 +0200

    Modified the first exercise:1) added the formatter for the resulting final price ; 2) added a do while condition in order to control that the input values are major to 0; 3) improved the general visibility of the code.

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -3,26 +3,29 @@ package org.lessons.java;
 import java.util.Scanner;
 
 public class CalcolaBiglietto {
-public static void main(String[] args) {
-	String kmToTravel;
-	String userAge;
-	
-	float ticketPriceMoltiplicatorUnit = 0.21f;
-	
-	Scanner in = new Scanner(System.in);
-	System.out.print("Quanti km dovrai percorrere? ");
-	kmToTravel = in.nextLine();
-	int km = Integer.parseInt(kmToTravel);			
-	float ticketPriceWithoutDiscount = km*ticketPriceMoltiplicatorUnit;
-	
-	System.out.print("Quanti anni hai ? ");
-	userAge = in.nextLine();
-	int userAgeNum =Integer.parseInt(userAge);
-	
-	float discount = (userAgeNum < 18) ? ticketPriceWithoutDiscount/100 * 20 : (userAgeNum > 65) ? ticketPriceWithoutDiscount/100 * 40 : 0;
-	float ticketPrice = ticketPriceWithoutDiscount - discount;
-	
-	System.out.println(ticketPrice);
-	in.close();
-}
+	public static void main(String[] args) {
+		int kmToTravel = 0;
+		int userAge = 0;
+		float ticketPriceMoltiplicatorUnit = 0.21f;
+		
+		Scanner in = new Scanner(System.in);
+		
+		do {
+			System.out.print("Quanti km dovrai percorrere? ");
+			kmToTravel = in.nextInt();	
+		}while(kmToTravel <= 0);
+		
+		float ticketPriceWithoutDiscount = kmToTravel * ticketPriceMoltiplicatorUnit;
+		
+		do {
+			System.out.print("Quanti anni hai ? ");
+			userAge = in.nextInt();
+		}while(userAge <= 0);
+		
+		float discount = (userAge < 18) ? ticketPriceWithoutDiscount/100 * 20 : (userAge > 65) ? ticketPriceWithoutDiscount/100 * 40 : 0;
+		float ticketPrice = ticketPriceWithoutDiscount - discount;
+		
+		System.out.println("Prezzo totale: " + String.format("%.2f",ticketPrice));
+		in.close();
+	}
 }
--- src/org/lessons/java/CibiPreferiti.java
+++ src/org/lessons/java/CibiPreferiti.java
@@ -1,17 +1,17 @@
 package org.lessons.java;
 
 public class CibiPreferiti {
-public static void main(String[] args) {
-	String[] favouriteFoods = {"pizza" , "pasta" , "lasagne" , "patatine" , "nutella"};
-	int favoutiteFoodsListLength = favouriteFoods.length;
-	System.out.println("Nella mia lista ho " + favoutiteFoodsListLength + "cibi preferiti");
-	System.out.println("Il mio cibo prefetito è: " + favouriteFoods[0]);
-	System.out.println("Il cibo che mi piace di meno è: " + favouriteFoods[favoutiteFoodsListLength - 1]);
-	
-	if(favoutiteFoodsListLength%2 != 0) {
-		System.out.println("Il cibo che sta nel mezzo è: " + favouriteFoods[favoutiteFoodsListLength/2]);
-	}else {
-		System.out.println("I cibi che stanno nel mezzo sono " + favouriteFoods[favoutiteFoodsListLength/2 - 1] + " e " + favouriteFoods[favoutiteFoodsListLength/2]);
+	public static void main(String[] args) {
+		String[] favouriteFoods = {"pizza" , "pasta" , "lasagne" , "patatine" , "nutella"};
+		int favoutiteFoodsListLength = favouriteFoods.length;
+		System.out.println("Nella mia lista ho " + favoutiteFoodsListLength + "cibi preferiti");
+		System.out.println("Il mio cibo prefetito è: " + favouriteFoods[0]);
+		System.out.println("Il cibo che mi piace di meno è: " + favouriteFoods[favoutiteFoodsListLength - 1]);
+		
+		if(favoutiteFoodsListLength%2 != 0) {
+			System.out.println("Il cibo che sta nel mezzo è: " + favouriteFoods[favoutiteFoodsListLength/2]);
+		}else {
+			System.out.println("I cibi che stanno nel mezzo sono " + favouriteFoods[favoutiteFoodsListLength/2 - 1] + " e " + favouriteFoods[favoutiteFoodsListLength/2]);
+		}
 	}
-}
 }
